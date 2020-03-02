7a8ac03160471927f10882a99920f0597612f902
Celeste Morton <creneemorton@gmail.com>
2020-03-02 09:44:24 -0800

    add container styling to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,24 @@ import SongDisplay from './SongDisplay';
 import SongList from './SongList';
 import Header from './Header';
 
-function App(){
+const container = {
+  paddingTop: '70px',
+  paddingRight: '55px',
+  paddingLeft: '55px',
+  marginRight: 'auto',
+  marginLeft: 'auto'
+}
+
+function App() {
   return (
     <div>
-      <Header />
-      <br/>
-      <SongList />
-      <hr/>
-      <SongDisplay />
+      <div style={container}>
+        <Header />
+        <br />
+        <SongList />
+        <hr />
+        <SongDisplay />
+      </div>
     </div>
   );
 }
